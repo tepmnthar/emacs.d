03dc621ca8c1c83278d6432a939f1f55213c9bd8
tepmnthar <TEP@tepmnthardeMacBook-Pro.local>
2016-08-14 04:30:42 +0800

    add JS packages

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,2 +1,45 @@
+;;;package
 (menu-bar-mode 0);
+(setq web-mode-enable-auto-closing t);
+(setq web-mode-enable-auto-quoting t);
+;;;(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(require 'js2-refactor) ;; Not necessary if using ELPA package
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "C-c C-m")
+;; eg. extract function with `C-c C-m ef`.
+
+(require 'web-beautify)
+(eval-after-load 'js2-mode
+  '(define-key js2-mode-map (kbd "C-c b") 'web-beautify-js))
+;; Or if you're using 'js-mode' (a.k.a 'javascript-mode')
+(eval-after-load 'js
+  '(define-key js-mode-map (kbd "C-c b") 'web-beautify-js))
+
+(eval-after-load 'json-mode
+  '(define-key json-mode-map (kbd "C-c b") 'web-beautify-js))
+
+(eval-after-load 'sgml-mode
+  '(define-key html-mode-map (kbd "C-c b") 'web-beautify-html))
+
+(eval-after-load 'web-mode
+  '(define-key web-mode-map (kbd "C-c b") 'web-beautify-html))
+
+(eval-after-load 'css-mode
+  '(define-key css-mode-map (kbd "C-c b") 'web-beautify-css))
+
+
+(add-hook 'js-mode-hook (lambda () (tern-mode t)))
+
+;;;(require 'company-backends)
+(eval-after-load 'company '(add-to-list 'company-backends 'company-tern))
+;;;(add-to-list 'company-backends 'company-tern)
 (provide 'init-local)
