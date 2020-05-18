ea46d162115b9d032357df9ed358a1a58aeedfd0
Zhang JC <632897649@qq.com>
2020-05-18 14:56:56 +0800

    增加 org-roam 配置

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -153,4 +153,37 @@
 
 (delete-selection-mode 1)
 
+;; org roam
+(use-package org-roam
+  :hook
+  (after-init . org-roam-mode)
+  :custom
+  (org-roam-directory "~/Documents/RoamNotes/")
+  (org-roam-completion-system 'ivy)
+  (org-roam-graph-viewer "open")
+  (org-roam-capture-templates '(("d" "default" plain (function org-roam--capture-get-point)
+                                 "%?"
+                                 :file-name "%<%Y%m%d%H%M%S>-${slug}"
+                                 :head "#+TITLE: ${title}\n"
+                                 :unnarrowed t)
+                                ("r" "references" plain (function org-roam--capture-get-point)
+                                 "%?"
+                                 :file-name "refs/%<%Y%m%d%H%M%S>-${slug}"
+                                 :head "#+TITLE: ${title}\n#+DATE: %t\n#+ROAM_KEY: %^{ROAM_KEY}\n"
+                                 :unnarrowed t)
+                                ("D" "drafts" plain (function org-roam--capture-get-point)
+                                 "%?"
+                                 :file-name "drafts/%<%Y%m%d%H%M%S>-${slug}"
+                                 :head "#+TITLE: ${title}\n#+DATE: %t\n"
+                                 :unnarrowed t)
+                                ))
+  :bind (:map org-roam-mode-map
+              (("C-c n l" . org-roam)
+               ("C-c n f" . org-roam-find-file)
+               ("C-c n j" . org-roam-jump-to-index)
+               ("C-c n b" . org-roam-switch-to-buffer)
+               ("C-c n g" . org-roam-graph))
+              :map org-mode-map
+              (("C-c n i" . org-roam-insert))))
+
 (provide 'init-local)
