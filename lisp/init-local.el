b206828ac560597c4ff9ff2f5f387b9b67a422cd
Zhang JC <632897649@qq.com>
2020-09-02 20:30:22 +0800

    换成 emacs-rime

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -23,67 +23,73 @@
   (package-install 'use-package))
 (require 'use-package)
 (require 'posframe)
-(use-package pyim
-  :ensure nil
-  :demand t
-  :config
+;; (use-package pyim
+;;   :ensure nil
+;;   :demand t
+;;   :config
 
-  ;; ;; 激活 basedict 拼音词库，五笔用户请继续阅读 README
-  ;; (use-package pyim-basedict
-  ;;   :ensure nil
-  ;;   :config (pyim-basedict-enable))
+;;   ;; ;; 激活 basedict 拼音词库，五笔用户请继续阅读 README
+;;   ;; (use-package pyim-basedict
+;;   ;;   :ensure nil
+;;   ;;   :config (pyim-basedict-enable))
 
-  ;; (setq default-input-method "pyim")
+;;   ;; (setq default-input-method "pyim")
 
-  ;; ;; 我使用全拼
-  ;; (setq pyim-default-scheme 'quanpin)
+;;   ;; ;; 我使用全拼
+;;   ;; (setq pyim-default-scheme 'quanpin)
 
-  ;; 激活 wbdict 拼音词库
-  (use-package pyim-wbdict
-    :ensure nil
-    :config (pyim-wbdict-v98-enable))
+;;   ;; 激活 wbdict 拼音词库
+;;   (use-package pyim-wbdict
+;;     :ensure nil
+;;     :config (pyim-wbdict-v98-enable))
 
-  (setq default-input-method "pyim")
+;;   (setq default-input-method "pyim")
 
-  ;; 我使用五笔
-  (setq pyim-default-scheme 'wubi)
+;;   ;; 我使用五笔
+;;   (setq pyim-default-scheme 'wubi)
 
-  ;; 设置 pyim 探针设置，这是 pyim 高级功能设置，可以实现 *无痛* 中英文切换 :-)
-  ;; 我自己使用的中英文动态切换规则是：
-  ;; 1. 光标只有在注释里面时，才可以输入中文。
-  ;; 2. 光标前是汉字字符时，才能输入中文。
-  ;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
-  (setq-default pyim-english-input-switch-functions
-                '(pyim-probe-dynamic-english
-                  pyim-probe-isearch-mode
-                  pyim-probe-program-mode
-                  pyim-probe-org-structure-template))
+;;   ;; 设置 pyim 探针设置，这是 pyim 高级功能设置，可以实现 *无痛* 中英文切换 :-)
+;;   ;; 我自己使用的中英文动态切换规则是：
+;;   ;; 1. 光标只有在注释里面时，才可以输入中文。
+;;   ;; 2. 光标前是汉字字符时，才能输入中文。
+;;   ;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
+;;   (setq-default pyim-english-input-switch-functions
+;;                 '(pyim-probe-dynamic-english
+;;                   pyim-probe-isearch-mode
+;;                   pyim-probe-program-mode
+;;                   pyim-probe-org-structure-template))
 
-  (setq-default pyim-punctuation-half-width-functions
-                '(pyim-probe-punctuation-line-beginning
-                  pyim-probe-punctuation-after-punctuation))
+;;   (setq-default pyim-punctuation-half-width-functions
+;;                 '(pyim-probe-punctuation-line-beginning
+;;                   pyim-probe-punctuation-after-punctuation))
 
-  ;; 开启拼音搜索功能
-  (pyim-isearch-mode 1)
+;;   ;; 开启拼音搜索功能
+;;   (pyim-isearch-mode 1)
 
-  ;; 使用 pupup-el 来绘制选词框, 如果用 emacs26, 建议设置
-  ;; 为 'posframe, 速度很快并且菜单不会变形，不过需要用户
-  ;; 手动安装 posframe 包。
-  ;; (setq pyim-page-tooltip 'posframe)
-  (setq pyim-use-tooltip 'posframe)
+;;   ;; 使用 pupup-el 来绘制选词框, 如果用 emacs26, 建议设置
+;;   ;; 为 'posframe, 速度很快并且菜单不会变形，不过需要用户
+;;   ;; 手动安装 posframe 包。
+;;   ;; (setq pyim-page-tooltip 'posframe)
+;;   (setq pyim-use-tooltip 'posframe)
 
-  ;; 选词框显示5个候选词
-  (setq pyim-page-length 5)
+;;   ;; 选词框显示5个候选词
+;;   (setq pyim-page-length 5)
 
-  :bind
-  (("M-j" . pyim-convert-string-at-point) ;与 pyim-probe-dynamic-english 配合
-   ("C-;" . pyim-delete-word-from-personal-buffer)))
+;;   :bind
+;;   (("M-j" . pyim-convert-string-at-point) ;与 pyim-probe-dynamic-english 配合
+;;    ("C-;" . pyim-delete-word-from-personal-buffer)))
 
 ;; 干掉 ivy minibuffer 的 M-j ivy-yank-word keybinding
 (use-package ivy
   :config
   (progn
     (define-key ivy-minibuffer-map (kbd "M-j") nil)))
+;; ;; isearch mode 里用中文
+;; (defun toggle-pyim-force-input-chinese ()
+;;   "切换 pyim 强制中文."
+;;   (interactive)
+;;   (setq pyim-force-input-chinese (not pyim-force-input-chinese)))
+;; (define-key isearch-mode-map (kbd "M-j") 'toggle-pyim-force-input-chinese)
 
 ;; (setq my-input-methods
 ;;       '("pyim")
@@ -145,6 +151,8 @@
 
 (setq org-reveal-root "file:///Users/tepmnthar/Development/reveal.js")
 
+(setq counsel-spotify-client-id "3e482bc54526410881b0de43c787d14e")
+(setq counsel-spotify-client-secret "71f3ff04a356400797b93ea58d8b51f9")
 
 (use-package counsel
   :defer t
@@ -230,5 +238,12 @@
         ("@Neutral" . (:foreground "Yellow" :weight bold))
         ("@None" . (:foreground "Gray" :weight bold))
         ))
+
+(require 'posframe)
+(use-package rime
+  :custom
+  (default-input-method "rime")
+  (rime-show-candidate 'posframe)
+  (rime-share-data-dir "~/Library/Rime"))
 
 (provide 'init-local)
