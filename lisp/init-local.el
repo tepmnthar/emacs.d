345d8f71f37d4a73f8eed3224ba9b95bc04d9d4b
Zhang JC <632897649@qq.com>
2023-01-05 19:21:00 +0800

    28.2适配

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -150,8 +150,7 @@
 
 ;; org roam
 (use-package org-roam
-  :hook
-  (after-init . org-roam-mode)
+  :ensure t
   :custom
   (org-roam-directory "~/Documents/RoamNotes/")
   (org-roam-completion-system 'ivy)
@@ -202,14 +201,19 @@
                                  :head "#+TITLE: ${title}\n#+DATE: %t\n#+STARTUP: showall\n#+OPTIONS: num:nil\n"
                                  :unnarrowed t)
                                 ))
-  :bind (:map org-roam-mode-map
-              (("C-c n l" . org-roam)
-               ("C-c n f" . org-roam-find-file)
-               ("C-c n j" . org-roam-jump-to-index)
-               ("C-c n b" . org-roam-switch-to-buffer)
-               ("C-c n g" . org-roam-graph))
-              :map org-mode-map
-              (("C-c n i" . org-roam-insert))))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
 
 (use-package org-journal
   :ensure t
