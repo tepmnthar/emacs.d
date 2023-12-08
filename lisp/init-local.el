7a040a2423b6e35cdae4e2f043d8df180f2b2a9e
Zhang JC <632897649@qq.com>
2023-12-08 19:20:49 +0800

    ellama

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -454,6 +454,18 @@ _h_   _l_   _o_k        _y_ank
 (use-package chatgpt-shell
   :ensure t
   :custom
-  ((chatgpt-shell-openai-key "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")))
+  ((chatgpt-shell-openai-key "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")))
+
+;; ellama
+(use-package ellama
+  :init
+  (setq ellama-language "Simplified Chinese")
+  (require 'llm-ollama)
+  (setq ellama-provider
+        ;; (make-llm-ollama
+        ;;  :chat-model "mistral:7b-text-q5_K_M" :embedding-model "mistral:7b-text-q5_K_M")
+        (make-llm-openai
+         :key (concat "sk-qVzE1MGB1yaTsBgld" "S62T3BlbkFJazdIgQ8FWA0NqI9PhDct") :chat-model "gpt-3.5-turbo")
+        ))
 
 (provide 'init-local)
